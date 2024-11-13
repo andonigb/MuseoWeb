100e67bf9ff12951f15ee99d5b0116f74887bf48
andonigb <andoni.guennoun@opendeusto.es>
2024-11-13 14:21:53 +0100

    dataBase inicio

--- myApp/admin.py
+++ myApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Usuario, Artista, Obras, Epoca, Museo  # Asegúrate de importar tus modelos
 
-# Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Artista)
+admin.site.register(Obras)
+admin.site.register(Epoca)
+admin.site.register(Museo)
+
--- myApp/models.py
+++ myApp/models.py
@@ -2,8 +2,53 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Usuario(models.Model):
-    id = models.AutoField(primary_key=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, default=1)
-    def __str__(self):
-        return self.user.username
+    id_usuario = models.IntegerField(primary_key=True)
+    usuario = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.usuario
+
+class Artista(models.Model):
+    id_artista = models.IntegerField(primary_key=True)
+    nom_artista = models.CharField(max_length=100)
+    pais_artista = models.CharField(max_length=100)
+    ruta_artista = models.CharField(max_length=100) #ruta a la imagen del artista
+    
+    def __str__(self):
+        return self.nom_artista
+    
+class Epoca(models.Model):
+    id_epoca = models.IntegerField(primary_key=True)
+    nom_epoca = models.CharField(max_length=100)
+    anyos_epoca = models.CharField(max_length=100)
+    ruta_epoca = models.CharField(max_length=100) #ruta a la imagen de la epoca
+
+    def __str__(self):
+        return self.nom_epoca
+
+class Museo(models.Model):
+    id_museo = models.IntegerField(primary_key=True)
+    nom_museo = models.CharField(max_length=100)
+    pais_museo = models.CharField(max_length=100)
+    ruta_museo = models.CharField(max_length=100) #ruta a la imagen del museo
+
+    def __str__(self):
+        return self.nom_museo
+
+class Obras(models.Model):
+    id_obra = models.IntegerField(primary_key=True)
+    nom_obra = models.CharField(max_length=100)
+    id_artista = models.ForeignKey(Artista, on_delete=models.CASCADE)
+    id_epoca = models.ForeignKey(Epoca, on_delete=models.CASCADE)
+    id_museo = models.ForeignKey(Museo, on_delete=models.CASCADE)
+    num_meGustas = models.IntegerField(default=0)
+    ruta_obra = models.CharField(max_length=100) #ruta a la imagen de la obra
+
+    def __str__(self):
+        return self.nom_obra
+
+class favoritas(models.Model):
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    id_obra = models.ForeignKey(Obras, on_delete=models.CASCADE)
